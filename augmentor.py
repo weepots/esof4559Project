76ddf7b19d92f55f7acd0d84c385922b0e5b6c9c
unknown <moelarabi400@gmail.com>
2023-11-02 02:56:19 +0100

    augmentor.py push

---
+++ augmentor.py
@@ -0,0 +1,21 @@
+import cv2
+import face_recognition
+import os
+
+
+def augment(changes, input_image_path, image_name, runs_directory):
+    test_image_to_change = cv2.imread(input_image_path)
+    ksize = (5, 5)
+    changed_image = test_image_to_change
+    alpha = 0.5  # Contrast control
+    beta = 10  # Brightness control
+    for change in changes:
+        if change == 'blur':
+            changed_image = cv2.blur(changed_image, ksize)
+        if change == 'low_exposure':
+            changed_image = cv2.convertScaleAbs(
+                changed_image, alpha=alpha, beta=beta)
+        # if change == 'noise':
+        # if change == 'cut off half image':
+    changed_image_path = os.path.join(runs_directory, image_name)
+    return changed_image, changed_image_path

30e9e3c944b71f45606a6a023f1afa7158659e5f
weepots <alexngrh107@gmail.com>
2023-11-02 00:06:44 -0400

    Fix augmentor py to use alpha only

--- augmentor.py
+++ augmentor.py
@@ -8,13 +8,12 @@ def augment(changes, input_image_path, image_name, runs_directory):
     ksize = (5, 5)
     changed_image = test_image_to_change
     alpha = 0.5  # Contrast control
-    beta = 10  # Brightness control
     for change in changes:
         if change == 'blur':
             changed_image = cv2.blur(changed_image, ksize)
-        if change == 'low_exposure':
+        if change == 'contrast':
             changed_image = cv2.convertScaleAbs(
-                changed_image, alpha=alpha, beta=beta)
+                changed_image, alpha=alpha)
         # if change == 'noise':
         # if change == 'cut off half image':
     changed_image_path = os.path.join(runs_directory, image_name)
